05f80add62df49528d73e1820d1a0a5179843f38
Nehemiah <NehemiahAK354@outlook.com>
2019-02-26 17:48:46 -0600

    Update enigmawires.js
    I am now setting down some variables that I will later have contain input from the user.

--- enigmawires.js
+++ enigmawires.js
@@ -1,1 +1,9 @@
+var userInput = "A"; // This is a test letter.  I will later give the user the control of what userInput is.
 
+var rotorOneSetting = 1; // This is a test number.  I will later give the user control of the value of rotorOneSetting.
+
+var rotorTwoSetting = 2; // This is a test number.  I will later give the user control of the value of rotorTwoSetting.
+
+var rotorThreeSetting = 3; // This is a test number.  I will later give the user control of the value of rotorThreeSetting.
+
+ 
