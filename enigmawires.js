596710716f519a2d85df59ab8c872558722e421a
Nehemiah <NehemiahAK354@outlook.com>
2019-02-27 11:33:56 -0600

    Update enigmawires.js
    Setting down some functions for HTML conditional attributes.

--- enigmawires.js
+++ enigmawires.js
@@ -3,15 +3,168 @@
  
 var userInput; 
  
-var rotorOneSetting; 
+var rotorOneSetting = document.getElementById("rotorOne").value; 
  
-var rotorTwoSetting; 
+var rotorTwoSetting = document.getElementById("rotorTwo").value; 
 
-var rotorThreeSetting;
+var rotorThreeSetting = document.getElementById("rotorThree").value;
  
+function PressedQKey() {
+
+ userInput = "Q";
+ 
+}
+ 
+function PressedWKey() {
+
+ userInput = "W";
+ 
+}
+ 
+ function PressedEKey() {
+
+ userInput = "E";
+ 
+}
+ 
+ function PressedRKey() {
+
+ userInput = "R";
+ 
+}
+ 
+ function PressedTKey() {
+
+ userInput = "T";
+ 
+}
+ 
+ function PressedYKey() {
+
+ userInput = "Y";
+ 
+}
+ 
+ function PressedUKey() {
+
+ userInput = "U";
+ 
+}
+ 
+ function PressedIKey() {
+
+ userInput = "I";
+ 
+}
+ 
+ function PressedOKey() {
+
+ userInput = "O";
+ 
+}
+ 
+ function PressedPKey() {
+
+ userInput = "P";
+ 
+}
+ 
+ function PressedAKey() {
+
+ userInput = "A";
+ 
+}
+ 
+ function PressedSKey() {
+
+ userInput = "S";
+ 
+}
+ 
+ function PressedDKey() {
+
+ userInput = "D";
+ 
+}
+ 
+ function PressedFKey() {
+
+ userInput = "F";
+ 
+}
+ 
+ function PressedGKey() {
+
+ userInput = "G";
+ 
+}
+ 
+ function PressedHKey() {
+
+ userInput = "H";
+ 
+}
+ 
+ function PressedJKey() {
+
+ userInput = "J";
+ 
+}
+ 
+ function PressedKKey() {
+
+ userInput = "K";
+ 
+}
+ 
+ function PressedLKey() {
+
+ userInput = "L";
+ 
+}
+ 
+ function PressedZKey() {
+
+ userInput = "Z";
+ 
+}
+ 
+ function PressedXKey() {
+
+ userInput = "X";
+ 
+}
+ 
+ function PressedCKey() {
+
+ userInput = "C";
+ 
+}
+ 
+ function PressedVKey() {
+
+ userInput = "V";
+ 
+}
+ 
+ function PressedBKey() {
+
+ userInput = "B";
+ 
+}
+ 
+ function PressedNKey() {
+
+ userInput = "N";
+ 
+}
+ 
+ function PressedMKey() {
+
+ userInput = "M";
+ 
+}
 
  }());
 
 enigma();
-
- 
