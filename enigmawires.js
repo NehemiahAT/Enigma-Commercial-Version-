400b2fc4242fded152a2f9a7738c1d5bf4ee9367
Nehemiah <NehemiahAK354@outlook.com>
2019-02-27 10:46:55 -0600

    Update enigmawires.js

--- enigmawires.js
+++ enigmawires.js
@@ -1,9 +1,17 @@
-var userInput = "A"; // This is a test letter.  I will later give the user the control of what userInput is.
+(function enigma() {
+ "use strict";
+ 
+var userInput; 
+ 
+var rotorOneSetting; 
+ 
+var rotorTwoSetting; 
 
-var rotorOneSetting = 1; // This is a test number.  I will later give the user control of the value of rotorOneSetting.
+var rotorThreeSetting;
+ 
 
-var rotorTwoSetting = 2; // This is a test number.  I will later give the user control of the value of rotorTwoSetting.
+ }());
 
-var rotorThreeSetting = 3; // This is a test number.  I will later give the user control of the value of rotorThreeSetting.
+enigma();
 
  
